55485cc4c15dfae6e68310b293c6599c0c55815c
any-gomes <any.gomes@outlook.com.br>
2021-11-22 20:25:58 -0300

    correçoes

--- users/models.py
+++ users/models.py
@@ -2,7 +2,8 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 from composteira.models import models
-#from classificacao.models import models
+from classificacao.models import models
+from notificacaoMeta.models import models
 
 
 class User(AbstractUser):
--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
 
+from .models import models
+from insumo import Insumo
 # Create your views here.
+
+def novoInsumo(request):
+    nInsumo = Insumo()
+    return render(request,'account/addinsumo.html', {'insumo' : Insumo})
+
+def novoInsumo(request):
+    nInsumo = Insumo()
+    return render(request,'account/addinsumo.html', {'insumo' : Insumo})
