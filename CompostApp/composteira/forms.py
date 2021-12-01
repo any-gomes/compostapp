5724ba5177b44718c547d688731666c42b377024
Rafael Ferreira <61996187+rafael3021734@users.noreply.github.com>
2021-12-01 20:04:35 -0300

    Adicionado campos do formulário

--- CompostApp/composteira/forms.py
+++ CompostApp/composteira/forms.py
@@ -6,7 +6,10 @@ from composteira.models import Composteira
 class ComposteiraForms(forms.Form):
         model = Composteira
         nome = forms.CharField(max_length=30)
-
+        data_inicio_comp = forms.DateField(required=True)
+        tamanho_comp = forms.IntegerField(required=True)
+        data_conclusao_comp = forms.DateField(required=True)
+        #insumo = forms.ManyToManyField(required=True)
        # fields = '__all__'
        # fields = ('nome', 'tamanho_comp', 'humus_produzido')
 
