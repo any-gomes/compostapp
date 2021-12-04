01007e0fedfc1180f0d902adacbf3b4e8d9b7052
Rafael Ferreira <61996187+rafael3021734@users.noreply.github.com>
2021-12-04 01:24:16 -0300

    alterado fields (retirado=Insumo)
    Foi retirado o atributo insumo e comentado o código anterior

--- CompostApp/composteira/forms.py
+++ CompostApp/composteira/forms.py
@@ -4,5 +4,6 @@ from .models import Composteira
 class ComposteiraForms(forms.ModelForm):
         class Meta:
                 model = Composteira
-                fields = ('nome', 'tamanho_comp', 'humus_produzido', 'insumo')
+                fields = ('nome', 'tamanho_comp', 'humus_produzido')
+                #fields = ('nome', 'tamanho_comp', 'humus_produzido', 'insumo')
 
